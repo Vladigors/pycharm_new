96cda7437e98b35039d396164bc72b743ec01d28
Vladigors <83923210+Vladigors@users.noreply.github.com>
2021-06-06 18:12:24 +0300

    Update nums_1.py
    Change nums in code, add:         for i in range(10, 14):
                                                           print(i, end= '')

--- nums1.py
+++ nums1.py
@@ -2,9 +2,11 @@ range(10)
 list(range(1, 10))
 for i in range(10):
     print(i, end= '')
-for i in range(3, 20):
+for i in range(3, 11):
     print(i, end= '')
-for i in range (5, 25, 3):
+for i in range(5, 12, 3):
     print(i, end= '')
-for i in range(10, 20):
+for i in range(10, 13):
+    print(i, end= '')
+for i in range(10, 14):
     print(i, end= '')
