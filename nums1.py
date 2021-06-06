991cdeaa7acd824ea479b3411bd85aad87d0e3b2
Vladigors <83923210+Vladigors@users.noreply.github.com>
2021-06-06 18:08:04 +0300

    Add files via upload

---
+++ nums1.py
@@ -0,0 +1,10 @@
+range(10)
+list(range(1, 10))
+for i in range(10):
+    print(i, end= '')
+for i in range(3, 20):
+    print(i, end= '')
+for i in range (5, 25, 3):
+    print(i, end= '')
+for i in range(10, 20):
+    print(i, end= '')
